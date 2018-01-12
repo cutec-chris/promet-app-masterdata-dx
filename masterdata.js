091aad73117d864f8c3f6c8aef1461a0b0f1e58b
Chris <ulrich@tcs-germany.de>
2018-01-12 15:15:38 +0100

    inistal commit

---
+++ masterdata.js
@@ -0,0 +1,10 @@
+var Masterdata;
+dhtmlxEvent(window,"load",function(){
+  Masterdata = newPrometList('masterdata','Stammdaten');
+  Masterdata.Grid.setHeader(["Nummer","Kurztext","Version","Status","Kategorie"]);
+  Masterdata.Grid.setColumnIds('ID,SHORTTEXT,VERSION,STATUS,CATEGORY')
+  Masterdata.Grid.setColTypes("ro,ro,ro,ro,ro");
+  Masterdata.Grid.attachHeader("#text_filter,#text_filter,#text_filter,#select_filter,#text_filter");
+  Masterdata.Grid.setInitWidths('150,*,70,100,150');
+  Masterdata.Grid.init();
+});
