2af3d71588f392cb5bcf56f255cccd4137daa41e
Chris <ulrich@tcs-germany.de>
2018-07-19 15:19:18 +0200

    add Structure Tab

--- masterdata.js
+++ masterdata.js
@@ -1,7 +1,51 @@
-﻿rtl.module("masterdata",["System","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils"],function () {
+﻿rtl.module("masterdata",["System","JS","Web","Classes","Avamm","webrouter","AvammForms","dhtmlx_base","SysUtils","dhtmlx_treeview"],function () {
   "use strict";
   var $mod = this;
   rtl.createClass($mod,"TMasterdataForm",pas.AvammForms.TAvammForm,function () {
+    this.$init = function () {
+      pas.AvammForms.TAvammForm.$init.call(this);
+      this.FStructureTree = null;
+    };
+    this.$final = function () {
+      this.FStructureTree = undefined;
+      pas.AvammForms.TAvammForm.$final.call(this);
+    };
+    this.FillStructure = function (root, aData) {
+      var aList = null;
+      var aArt = null;
+      var i = 0;
+      var aText = "";
+      aList = rtl.getObject(aData["Data"]);
+      for (var $l1 = 0, $end2 = aList.length - 1; $l1 <= $end2; $l1++) {
+        i = $l1;
+        aArt = rtl.getObject(aList[i]);
+        aText = "" + aArt["SHORTTEXT"];
+        if (("" + aArt["IDENT"]) !== "") aText = ((aText + " (") + ("" + aArt["IDENT"])) + ")";
+        if (("" + aArt["VERSION"]) !== "") aText = ((aText + " [") + ("" + aArt["VERSION"])) + "]";
+        this.FStructureTree.addItem(aArt["sql_id"],aText,root);
+        var $tmp3 = "" + aArt["POSTYP"];
+        if ($tmp3 === "M") {
+          this.FStructureTree.setItemIcons(aArt["sql_id"],pas.JS.New(["file","fa-box","folder-opened","fa-box","folder-closed","fa-box"]))}
+         else if ($tmp3 === "A") {
+          this.FStructureTree.setItemIcons(aArt["sql_id"],pas.JS.New(["file","fa-cog","folder-opened","fa-cog","folder-closed","fa-cog"]))}
+         else if ($tmp3 === "X") this.FStructureTree.setItemIcons(aArt["sql_id"],pas.JS.New(["file","fa-comments","folder-opened","fa-comments","folder-closed","fa-comments"]));
+      };
+    };
+    this.DoLoadData = function () {
+      this.CreateForm();
+      pas.AvammForms.TAvammForm.DoLoadData.call(this);
+      this.DoOpen();
+    };
+    this.CreateForm = function () {
+    };
+    this.DoOpen = function () {
+      this.Tabs.addTab("structure",rtl.getResStr(pas.masterdata,"strStructure"),null,1,true,false);
+      this.FStructureTree = rtl.getObject(this.Tabs.cells("structure").attachTreeView(pas.JS.New([])));
+      this.FStructureTree.addItem("root",this.FData["SHORTTEXT"]);
+      this.FStructureTree.setIconset("font_awesome");
+      this.FillStructure("root",rtl.getObject(this.FData["MDPOSITIONS"]));
+      this.FStructureTree.openItem("root");
+    };
   });
   this.Masterdata = null;
   this.ShowMasterdata = function (URl, aRoute, Params) {
@@ -16,13 +60,13 @@
       var $with1 = $mod.Masterdata;
       $with1.Grid.setHeader("Nummer,Kurztext,Version,Status,Kategorie");
       $with1.Grid.setColumnIds("ID,SHORTTEXT,VERSION,STATUS,CATEGORY");
-      $with1.Grid.attachHeader("#text_filter,#text_filter,#text_filter,#select_filter,#text_filter");
+      $with1.SetFilterHeader("#text_filter,#text_filter,#text_filter,#select_filter,#text_filter");
       $with1.Grid.setInitWidths("150,*,70,100,150");
       $with1.Grid.init();
     };
     $mod.Masterdata.Show();
   };
-  $mod.$resourcestrings = {strMasterdata: {org: "Artikel"}};
+  $mod.$resourcestrings = {strMasterdata: {org: "Artikel"}, strStructure: {org: "Struktur"}};
   $mod.$init = function () {
     if (pas.Avamm.getRight("masterdata") > 0) pas.Avamm.RegisterSidebarRoute(rtl.getResStr(pas.masterdata,"strMasterdata"),"masterdata",$mod.ShowMasterdataList);
     pas.webrouter.Router().RegisterRoute("\/masterdata\/by-id\/:Id\/",$mod.ShowMasterdata,false);
--- masterdata.lpr
+++ masterdata.lpr
@@ -1,16 +1,26 @@
 library masterdata;
   uses js, web, classes, Avamm, webrouter, AvammForms, dhtmlx_base,
-    dhtmlx_form,SysUtils, Types;
+    dhtmlx_form,SysUtils, Types, dhtmlx_treeview;
 
 type
 
   { TStatisticsForm }
 
+  { TMasterdataForm }
+
   TMasterdataForm = class(TAvammForm)
+  private
+    FStructureTree : TDHTMLXTreeview;
+    procedure FillStructure(root : JSValue;aData : TJSObject);
+  public
+    procedure DoLoadData; override;
+    procedure CreateForm;
+    procedure DoOpen;
   end;
 
 resourcestring
   strMasterdata             = 'Artikel';
+  strStructure              = 'Struktur';
 
 var
   Masterdata : TAvammListForm = nil;
@@ -32,12 +42,73 @@ begin
         begin
           Grid.setHeader('Nummer,Kurztext,Version,Status,Kategorie');
           Grid.setColumnIds('ID,SHORTTEXT,VERSION,STATUS,CATEGORY');
-          Grid.attachHeader('#text_filter,#text_filter,#text_filter,#select_filter,#text_filter');
+          FilterHeader := '#text_filter,#text_filter,#text_filter,#select_filter,#text_filter';
           Grid.setInitWidths('150,*,70,100,150');
           Grid.init();
         end;
     end;
   Masterdata.Show;
+end;
+
+{ TMasterdataForm }
+
+procedure TMasterdataForm.FillStructure(root: JSValue; aData: TJSObject);
+var
+  aList: TJSArray;
+  aArt: TJSObject;
+  i: Integer;
+  aText: String;
+begin
+  aList := TJSArray(aData.Properties['Data']);
+  for i := 0 to aList.Length-1 do
+    begin
+      aArt := TJSObject(aList.Elements[i]);
+      aText := string(aArt.Properties['SHORTTEXT']);
+      if string(aArt.Properties['IDENT']) <> '' then
+        aText := aText+' ('+string(aArt.Properties['IDENT'])+')';
+      if string(aArt.Properties['VERSION'])<>'' then
+        aText := aText+' ['+string(aArt.Properties['VERSION'])+']';
+      FStructureTree.addItem(aArt.Properties['sql_id'],aText,root);
+      case string(aArt.Properties['POSTYP']) of
+      'M':
+        FStructureTree.setItemIcons(aArt.Properties['sql_id'],js.new(['file','fa-box',
+                                                                    'folder-opened','fa-box',
+                                                                    'folder-closed','fa-box'
+                                                                   ]));
+      'A':
+        FStructureTree.setItemIcons(aArt.Properties['sql_id'],js.new(['file','fa-cog',
+                                                                    'folder-opened','fa-cog',
+                                                                    'folder-closed','fa-cog'
+                                                                   ]));
+      'X':
+        FStructureTree.setItemIcons(aArt.Properties['sql_id'],js.new(['file','fa-comments',
+                                                                    'folder-opened','fa-comments',
+                                                                    'folder-closed','fa-comments'
+                                                                   ]));
+      end;
+    end;
+end;
+
+procedure TMasterdataForm.DoLoadData;
+begin
+  CreateForm;
+  inherited DoLoadData;
+  DoOpen;
+end;
+
+procedure TMasterdataForm.CreateForm;
+begin
+
+end;
+
+procedure TMasterdataForm.DoOpen;
+begin
+  Tabs.addTab('structure',strStructure,nil,1,true,false);
+  FStructureTree := TDHTMLXTreeview(Tabs.cells('structure').attachTreeView(js.new([])));
+  FStructureTree.addItem('root',Data.Properties['SHORTTEXT']);
+  FStructureTree.setIconset('font_awesome');
+  FillStructure('root',TJSObject(Data.Properties['MDPOSITIONS']));
+  FStructureTree.openItem('root');
 end;
 
 initialization
